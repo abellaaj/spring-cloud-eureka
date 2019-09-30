963b38f246048beb27c35f1aada3e3193673567a
BELLAAJ Aiman <Aiman.BELLAAJ_EXT@bpce.fr>
2019-09-30 17:37:06 +0200

    second commit

---
+++ spring-cloud-eureka-client/src/main/java/fr/aiman/spring/cloud/eureka/client/GreetingController.java
@@ -0,0 +1,8 @@
+package fr.aiman.spring.cloud.eureka.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+    @RequestMapping("/greeting")
+    String greeting();
+}
